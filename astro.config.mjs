d8fad92f6e5c5ffc0c06f94167c57dc785ae96de
Chantepierre <lucas@lucassifoni.info>
2025-01-30 13:35:17 +0100

    Fixes RSS & Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lucassifoni.info',
-  integrations: [mdx(), vue()],
+  site: "https://lucassifoni.info",
+  integrations: [mdx(), vue(), sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'github-light'
-    }
-  }
+      theme: "github-light",
+    },
+  },
 });
