98b937d625c54dcfae925bf321471491d1753166
Chantepierre <lucas@lucassifoni.info>
2025-04-25 09:53:17 +0200

    Improves tag density

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,28 @@
 import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 
-export function GET() {
+export async function GET(context) {
+  const posts = await Promise.all(
+    Object.entries(import.meta.glob("./blog/**/*.{md,mdx}", { eager: true }))
+      .map(async ([path, post]) => {
+        return {
+          link: path.replace(/^\.\/blog\/(.*)\.mdx?$/, "/blog/$1"),
+          title: post.frontmatter?.title,
+          pubDate: post.frontmatter?.pubDate,
+          description: post.frontmatter?.description,
+          customData: post.frontmatter?.tags 
+            ? `<category>${post.frontmatter.tags.join("</category><category>")}</category>` 
+            : "",
+        };
+      })
+  );
+  
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: import.meta.env.SITE,
-    items: import.meta.glob("./blog/**/*.{md,mdx}"),
+    site: context.site,
+    items: posts,
+    customData: `<language>en-us</language>`,
+    stylesheet: '/rss/styles.xsl',
   });
 }
