9e01c9cb772536e9af2c750daa0ce129f0053f5e
Chantepierre <lucas@lucassifoni.info>
2026-03-12 17:04:59 +0100

    DFTFringe talk

---
+++ public/talks/rougail-solstice-live-dftfringe/assets/slidev/default-DzZ4x8WG.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-DBEVJtFO.js";import{_ as r}from"../index-BA877K9l.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
